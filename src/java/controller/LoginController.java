568d38a916e26f82ec41edd841ae6f797ec3e61c
WuWuyn <vukhanhhuyengt30@gmail.com>
2025-04-16 10:58:49 +0700

    hehe

---
+++ src/java/controller/LoginController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ * @author admin
+ */
+public class LoginController extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String user = req.getParameter("username");
+        String pass = req.getParameter("password");
+        
+        if(user.equals("jawuyn") && pass.equals("16042023")){
+            resp.sendRedirect("index.html");
+        } else{
+            req.setAttribute("error", "Bakaaaaa!!! Đồ tòy:<<");
+            req.getRequestDispatcher("login.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("login.jsp").forward(req, resp);
+    }
+    
+}
